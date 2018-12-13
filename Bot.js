883c7ea807427b24c21e252a46b9b346b2d2c1ff
Spam127 <45201082+Spam127@users.noreply.github.com>
2018-12-13 13:54:22 +0200

    Add files via upload

---
+++ Bot.js
@@ -0,0 +1,109 @@
+﻿const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+client.on('message', message => { 
+let PREFIX = '!'
+    if (message.content.startsWith(PREFIX + 'emojilist')) {
+
+        const List = message.guild.emojis.map(e => e.toString()).join(" ");
+
+        const EmojiList = new Discord.RichEmbed()
+            .setTitle('➠ Emojis') 
+            .setAuthor(message.guild.name, message.guild.iconURL) 
+            .setColor('RANDOM') 
+            .setDescription(List) 
+            .setFooter(message.guild.name) 
+        message.channel.send(EmojiList) 
+    }
+});
+
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+
+client.on('ready', () => {
+    client.channels.get("520985093742002205").join(); 
+    });
+
+client.on("message",(message) => {
+if(message.content.startsWith("botserver")){
+    let embed = new Discord.RichEmbed()
+.setAuthor(client.guild.name,client.guild.iconURL)
+.setThumbnail(client.guild.iconURL)
+.addField('Server Name',client.guild.name)
+.addField('Server Member Count',client.guild.members.size)
+.addField(`Id Server`,`${client.guild.id}`)
+.addField(`Owner Server`,`${client.guild.owner}`)
+.setColor('RANDOM')
+message.channel.sendEmbed(embed);
+}
+});
+
+var antispam = require("anti-spam");//npm i anti-spam
+ 
+antispam(client, {
+  warnBuffer: 3, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
+  maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
+  interval: 1000, // مقدار الوقت قبل حصول باند
+  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
+  roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
+  roleName: "Muted", // اسم رتبة الميوت
+  maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
+  maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
+  time: 10, // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
+});
+
+client.on('message',async message => {
+  if(message.content === '+unbanall') {
+    var user = message.mentions.users.first();
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك صلاحية `**');
+    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    const guild = message.guild;
+
+  message.guild.fetchBans().then(ba => {
+  ba.forEach(ns => {
+  message.guild.unban(ns);
+  const embed= new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor("Succes!", "https://images-ext-1.discordapp.net/external/vp2vj9m0ieU5J6SHg6ObIsGpTJyoZnGAebrd0_vi848/https/i.imgur.com/GnR2unD.png?width=455&height=455")
+        .setDescription(`**:white_check_mark: Has Been Unban For All**`)
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+  message.channel.sendEmbed(embed);
+  guild.owner.send(`سيرفر : ${guild.name}
+  **تم فك الباند عن الجميع بواسطة** : <@${message.author.id}>`) 
+  });
+  });
+  }
+  });
+  
+  bot.on('message', message => {
+  if(message.author.bot || message.channel.type == 'dm') return;
+  let prefix = '!';
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+  if(command == 'bc') {
+    if(message.member.hasPermission('ADMINISTRATOR')) {
+      if(args.join(' ')) {
+        message.channel.send(`Sending to all members...`).then(async m => {
+          message.guild.members.forEach(async member => {
+
+            member.send(args.join(' ')).catch(err => {
+              if(err) console.log(`Couldn't send message to this member, ${member.user.tag}`)
+            });
+          });
+        });
+      } else return message.reply('!bc Message');
+    } else return;
+  }
+});
+
+bot.login('tokeen!')
